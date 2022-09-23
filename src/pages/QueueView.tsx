6c93c68fc9a00643bc22e6f9644c6a24997e18c9
jbc2313 <jamesbcorbin@gmail.com>
2022-09-22 22:04:55 -0500

    added queue

---
+++ src/pages/QueueView.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+
+
+const QueueView = () => {
+
+
+    return (
+        <h2>Queue for doggos will be here</h2>
+    )
+
+}
+
+export default QueueView;
---
+++ src/util/queueClass.ts
@@ -0,0 +1,38 @@
+
+interface TypeQueue<T> {
+    enqueue(item: T): void;
+    dequeue(): T | undefined;
+    size(): number;
+}
+
+
+class Queue<T> implements TypeQueue<T> {
+    private storage: T[] = [];
+
+    constructor(private capacity: number = Infinity) {}
+
+    enqueue(item: T): void{
+        if (this.size() === this.capacity) {
+            throw Error("Queue has reach max capacity")
+        }
+        this.storage.push(item);
+    }
+
+    dequeue(): T | undefined {
+        return this.storage.shift();
+    }
+
+    size(): number {
+        return this.storage.length;
+    }
+    
+    list(): T[] | undefined {
+        return this.storage;
+    }
+
+
+}
+
+
+
+export const dogqueue = new Queue<string>
