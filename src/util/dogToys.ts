b6b811f20462ef6e0e3493939811dded4859c2bf
jb corbin <101614021+jbc2313@users.noreply.github.com>
2022-12-10 17:40:53 -0600

    added dogtoys

--- src/util/dogToys.ts
+++ src/util/dogToys.ts
@@ -1,7 +1,28 @@
+import { randProgrammingLanguage } from '@ngneat/falso'
 
+interface DogToy {
+    language: string;
+    used: boolean;
+}
+
+type DogName = "Darrel" | "Boris" | "Toby";
+
+const langOne = randProgrammingLanguage();
+const langTwo = randProgrammingLanguage();
+const langThree = randProgrammingLanguage();
+
+
+const dogs: Record<DogName, DogToy> = {
+    Darrel: { language: langOne, used: true },
+    Boris: { language: langTwo, used: false },
+    Toby: { language: langThree, used: true },
+}
 
 export function logSent() {
 
+    console.log(`Dog 1: ${dogs.Darrel}`);
+    console.log(`Dog 2: ${dogs.Boris}`);
+    console.log(`Dog 3: ${dogs.Toby}`);
 
 
 }
