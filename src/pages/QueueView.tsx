1f5bc1d4868c402378105ddf737abc2bb415b63f
jb corbin <101614021+jbc2313@users.noreply.github.com>
2023-05-20 16:10:52 -0500

    added banner below doggy queue

--- src/pages/QueueView.tsx
+++ src/pages/QueueView.tsx
@@ -7,10 +7,10 @@ const QueueView = () => {
     return (
         <div>
         <h2><u>Doggo Queue</u></h2>
+        <h3>A Queue for Doggos!</h3>
         <QueueMain />
         </div>
     )
-
 }
 
 export default QueueView;
