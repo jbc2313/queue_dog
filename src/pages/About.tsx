63992b70835862a4bc0759e3f4bb087d0508d550
jb <101614021+jbc2313@users.noreply.github.com>
2023-06-17 09:50:00 -0500

    updated about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -7,7 +7,7 @@ return (
         <h2>Why Queues and Stacks?</h2>
         <p>I built this simply for fun. I wanted to learn more about stacks and queues. So here we are!</p>
         <h2>Why React?</h2>
-        <p>When I started this project I did not feel like learning another JS framework. I am familiar with React, Create React App, Express.js and Next.js. Now I am open to learn any framework and/or language.</p>
+        <p>When I started this project I did not feel like learning another JS framework. I am familiar with React, Create React App, Express.js and Next.js. Now I am much more open to learning another JS framework or even another method for web developement in a different language.</p>
 
     </div>
 
